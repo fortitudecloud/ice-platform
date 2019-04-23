f12e16d814d964b13ae98a3d2661c40bf4de9d53
Lionel Hickey <lionel.hickey@gmail.com>
2019-04-23 13:13:41 +1000

    Added request pipeline

--- ICE.Platform/ICE.Platform/PlatformTriggerBase.cs
+++ ICE.Platform/ICE.Platform/PlatformTriggerBase.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,14 @@ namespace ICE.Platform
     public abstract class PlatformTriggerBase<E> : IPlatformTrigger<E> where E : IPlatformEntity
     {
         public IPlatformContext Context { get; set; }
+        public IRequestMetadata Metadata { get; set; }
+
+        public Task AfterRead(IEnumerable<E> objects, Action<object> mutateResponse)
+        {
+            if (objects.Count() == 1) mutateResponse(resp => resp = objects.First());
+
+            return Task.Delay(0);
+        }
 
         public virtual Task BeforeCreate(IEnumerable<E> objects)
         {
