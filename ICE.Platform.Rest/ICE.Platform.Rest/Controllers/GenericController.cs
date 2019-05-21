7eaf4573080a7ed3c19e897f8db17ac8b1df102e
Lionel Hickey <lionel.hickey@sew.com.au>
2019-05-21 16:07:53 +1000

    added resource filter example

---
+++ ICE.Platform.Rest/ICE.Platform.Rest/Controllers/GenericController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ICE.Platform.Rest.Controllers
+{
+    using Filters;
+
+    [Route("generic/{entity}")]
+    [ApiController]
+    [BeforeRequestFilter]
+    public class GenericController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public ActionResult Get(string id)
+        {
+            return new JsonResult(new { status = true });
+        }
+    }
+}
---
+++ ICE.Platform.Rest/ICE.Platform.Rest/Filters/BeforeRequestFilter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ICE.Platform.Rest.Filters
+{
+    public class BeforeRequestFilter : Attribute, IResourceFilter
+    {
+        public void OnResourceExecuted(ResourceExecutedContext context)
+        {
+            // noop
+        }
+
+        public void OnResourceExecuting(ResourceExecutingContext context)
+        {
+            // noop
+        }        
+
+    }
+}
